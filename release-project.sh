202b510de3d5a04d4501a7d69832048d66de0ea1
Ryan Doherty <tech@conical.org>
2022-06-30 15:58:50 -0400

    Move files from base-pom; better to have in a separate repo so we can seamlessly use them to release base-pom as well

---
+++ release-project.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ "$GITHUB_USERNAME" == "" ]; then
+    echo 'Environment variable $GITHUB_USERNAME must be set'
+    exit 1
+fi
+if [ "$GITHUB_TOKEN" == "" ]; then
+    echo 'Environment variable $GITHUB_TOKEN must be set'
+    exit 1
+fi
+echo "Github credentials for $GITHUB_USERNAME found in environment"
+
+echo "Fetching settings.xml" \
+     && curl -O https://raw.githubusercontent.com/VEuPathDB/maven-release-tools/main/settings.xml \
+     && echo "Preparing release..." \
+     && mvn -Dusername=git release:prepare \
+     && echo "Prepare successful; deploying artifacts" \
+     && mvn release:perform --settings ./settings.xml \
+     && echo "Release succeeded. Cleaning up." \
+     && git push \
+     && rm -f release-project.sh settings.xml
+
+echo "Done."
