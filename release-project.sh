926febda39aca6ab0ba301b0e845f1c50b4605d4
Ryan Doherty <tech@conical.org>
2025-07-09 06:43:41 -0400

    Add pre-release build testing to avoid mid-release failures

--- release-project.sh
+++ release-project.sh
@@ -12,6 +12,10 @@ echo "Github credentials for $GITHUB_USERNAME found in environment"
 
 echo "Fetching settings.xml" \
      && curl -O https://raw.githubusercontent.com/VEuPathDB/maven-release-tools/main/settings.xml \
+     && echo "Testing build..." \
+     && mvn clean package \
+     && echo "Testing JavaDoc..." \
+     && mvn javadoc:javadoc \
      && echo "Preparing release..." \
      && mvn -Dusername=git release:prepare \
      && echo "Prepare successful; deploying artifacts" \
